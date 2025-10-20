b4d68b5e8c3fbad3e102ba0d0487568195f0a752
Abderrahmanedev23 <125931479+Abderrahmanedev23@users.noreply.github.com>
2025-10-20 01:11:31 +0100

    first commit

---
+++ src/main/java/org/mabchour/bankaccountmicroservice/BankAccountMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.mabchour.bankaccountmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mabchour/bankaccountmicroservice/BankAccountMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mabchour.bankaccountmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
