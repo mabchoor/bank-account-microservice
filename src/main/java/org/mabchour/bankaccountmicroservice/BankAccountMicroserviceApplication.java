29eec84533eeb3a1fe15b6180f406d727cb5a0ae
Abderrahmanedev23 <125931479+Abderrahmanedev23@users.noreply.github.com>
2025-10-20 01:12:34 +0100

    initial commit

--- src/main/java/org/mabchour/bankaccountmicroservice/BankAccountMicroserviceApplication.java
+++ src/main/java/org/mabchour/bankaccountmicroservice/BankAccountMicroserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankAccountMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankAccountMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountMicroserviceApplication.class, args);
+    }
 
 }
